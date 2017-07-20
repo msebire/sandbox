7b313b0a60d3011fe390e0c33fb5ad53f1d8f25b
xfournet <xfournet@users.noreply.github.com>
2017-07-21 00:14:12 +0200

    Fix build failure detection

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -3,16 +3,16 @@
 if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
     echo
     echo "Decrypting GPG files"
-    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/pubring.gpg.enc -out ci/pubring.gpg
-    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/secring.gpg.enc -out ci/secring.gpg
+    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/pubring.gpg.enc -out ci/pubring.gpg || exit 1
+    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/secring.gpg.enc -out ci/secring.gpg || exit 1
 
     echo
     echo "Build, test and generate Sonar report"
-    mvn -B clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
+    mvn -B clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar || exit 1
 
     echo
     echo "Build and sign with GPG"
-    mvn -B -P release -DskipTests=true -s ./ci/settings.xml clean verify
+    mvn -B -P release -DskipTests=true -s ./ci/settings.xml clean verify || exit 1
 
     echo
     echo "Build output"
