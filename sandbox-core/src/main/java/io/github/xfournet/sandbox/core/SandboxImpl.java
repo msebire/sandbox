9891c3d3f2480c0cd8bf6ccf4f28c43ddad2db96
xfournet <xfournet@users.noreply.github.com>
2017-07-21 00:16:01 +0200

    Fix build

--- sandbox-core/src/main/java/io/github/xfournet/sandbox/core/SandboxImpl.java
+++ sandbox-core/src/main/java/io/github/xfournet/sandbox/core/SandboxImpl.java
@@ -4,7 +4,7 @@ import io.github.xfournet.sandbox.Sandbox;
 
 public class SandboxImpl implements Sandbox {
     @Override
-    public String sayHllo(String name) {
+    public String sayHello(String name) {
         return String.format("Hello %s!", name);
     }
 }
