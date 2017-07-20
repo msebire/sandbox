949de7b2eadd613cc08e7e37ebb4d95ad42f5b4e
xfournet <xfournet@users.noreply.github.com>
2017-07-20 23:46:02 +0200

    Generate GPG signatures from Travis

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
+    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/pubring.gpg.enc -out ci/pubring.gpg
+    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/secring.gpg.enc -out ci/secring.gpg
     mvn -B clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
+    mvn -B -DskipTests=true -s ./ci/settings.xml clean verify
 else
-    mvn -B clean package -B
+    mvn -B clean verify
 fi
