e7066c9e86c4a4e3a93ccdec8201062ecff50b0c
xfournet <xfournet@users.noreply.github.com>
2017-07-21 02:51:33 +0200

    Update build files

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -2,15 +2,15 @@
 
 if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
     echo
-    echo "Decrypting GPG files"
-    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/pubring.gpg.enc -out ci/pubring.gpg || exit 1
-    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/secring.gpg.enc -out ci/secring.gpg || exit 1
-
-    echo
     echo "Build, test and generate Sonar report"
     mvn -B clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar || exit 1
 
     if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" -a "$OSSRH_USERNAME" != "" ]; then
+        echo
+        echo "Decrypting GPG files"
+        openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/pubring.gpg.enc -out ci/pubring.gpg || exit 1
+        openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/secring.gpg.enc -out ci/secring.gpg || exit 1
+
         echo
         echo "Build, sign with GPG and deploy to OSSRH"
         mvn -B -P publish -DskipTests=true -s ci/settings.xml clean deploy || exit 1
